1f6e256ad0828c65bd9c160cd2687feaa6d61765
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-17 18:00:13 +0100

    Add operate-function to route math operations
    Implements a wrapper function that accepts two numbers and an operator,
    then calls the appropriate math function. Includes a default case to
    handle invalid operators.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// New Part
 let number1;
 let operator;
 let number2;
+// New Part
+
 
 function add(a, b) {
     console.log(a+b);
@@ -16,4 +19,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
     console.log(a/b)
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+': 
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            throw new Error('Invalid operator');
+    }
 }
